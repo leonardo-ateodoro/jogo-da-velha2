96f122c47f6055fbbae6e07716aacb662ed991b0
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-08-28 10:31:28 -0300

    fdf

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Game from "./components/Game";
+import "./App.module.css";
+
+export default function App() {
+  return <Game />;
+}
---
+++ src/components/Game/index.jsx
@@ -0,0 +1,58 @@
+// Importa hooks, componentes e estilos
+import { useState } from "react";
+import Board from "../Board";
+import styles from "./Game.module.css";
+
+// Componente principal do jogo
+export default function Game() {
+  // Histórico de jogadas
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  // Movimento atual
+  const [currentMove, setCurrentMove] = useState(0);
+
+  // Define de quem é a vez (X começa)
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  // Atualiza o tabuleiro e o histórico
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  // Permite voltar para uma jogada anterior
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  // Lista de movimentos
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "🔄 Go to move #" + move;
+    } else {
+      description = "🎮 Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button className={styles.moveBtn} onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    );
+  });
+
+  // Renderiza o jogo
+  return (
+    <div className={styles.game}>
+      <div className={styles.boardWrapper}>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className={styles.info}>
+        <h2 className={styles.title}>History</h2>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
